48f27240c2d49e212bc267cd6dd6bd31a06d54d3
Thia100 <thegbolahanfathia@gmail.com>
2025-01-13 16:34:14 +0100

    second commit

--- srcipts/getdates.js
+++ srcipts/getdates.js
@@ -2,6 +2,6 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Dynamically update the last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+// // Dynamically update the last modified date
+// const lastModified = document.lastModified;
+// document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
