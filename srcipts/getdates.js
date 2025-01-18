6dd39ab9426cec3de3d25d9dffbc794d0941e7d0
Thia100 <thegbolahanfathia@gmail.com>
2025-01-18 12:12:42 +0100

    changed js

--- srcipts/getdates.js
+++
@@ -1,7 +0,0 @@
-// Dynamically update the current year
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
-
-// // Dynamically update the last modified date
-// const lastModified = document.lastModified;
-// document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
