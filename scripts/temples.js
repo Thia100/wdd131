399e44c1f01077471d84a7d3481942b796e4f2ac
Thia100 <thegbolahanfathia@gmail.com>
2025-01-26 11:33:32 +0100

    js

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,14 @@ mobileMenu.addEventListener('click', () => {
   navList.classList.toggle('active'); // Add/remove 'active' class to show/hide the menu
 });
 
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+
+// Output the current year in the footer's first paragraph
+document.getElementById('currentyear').textContent = `© ${currentYear}`;
+
+// Output the last modified date in the footer's second paragraph
+document.getElementById('last-modified').textContent = `Last modified: ${lastModifiedDate}`;
