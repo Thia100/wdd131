a73f7a479e080d0e70191361d1c6dc50d40b74be
Thia100 <thegbolahanfathia@gmail.com>
2025-02-01 09:29:41 +0100

    country page

--- scripts/sharp.js
+++
@@ -1,9 +0,0 @@
-import sharp from 'sharp';
-import { readdirSync } from 'fs';
-const directory = 'images/';
-
-readdirSync(directory).forEach(file => {
-  sharp(`${directory}/${file}`)
-    .resize(200, 100) // width, height
-    .toFile(`${directory}/${file}-small.jpg`);
-});
