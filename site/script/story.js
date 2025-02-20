e84aa0ca169df6f7b93e6a5cacf9cc24daa6d502
Thia100 <thegbolahanfathia@gmail.com>
2025-02-20 19:23:59 +0100

    second page

---
+++ site/script/story.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+function hideSidebar(){
+  const hidebar = document.querySelector(".sidebar");
+  hidebar.style.display = "none";
+}
--- site/script/warrior.js
+++ site/script/warrior.js
@@ -1,10 +1,10 @@
 function showSidebar(){
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.style.display = "flex";
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
-  function hideSidebar(){
-    const hidebar = document.querySelector(".sidebar");
-    hidebar.style.display = "none";
+function hideSidebar(){
+  const hidebar = document.querySelector(".sidebar");
+  hidebar.style.display = "none";
 }
 
 
