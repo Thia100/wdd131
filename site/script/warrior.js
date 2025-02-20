eb528d02c15ea4d2612b0f74f3b0d113fa14993a
Thia100 <thegbolahanfathia@gmail.com>
2025-02-20 01:41:00 +0100

    page 2

--- site/script/warrior.js
+++ site/script/warrior.js
@@ -9,4 +9,29 @@ function showSidebar(){
 
 
 
-  
+document.addEventListener("DOMContentLoaded", () => {
+  const selectDrop = document.querySelector("#countries");
+
+  fetch("https://restcountries.com/v3.1/all")
+    .then((response) => response.json())
+    .then((data) => {
+      
+      let output = `<option value="" disabled selected>Select a country</option>`;
+      
+      
+      data.sort((a, b) => {
+        const nameA = a.name.common.toUpperCase();
+        const nameB = b.name.common.toUpperCase();
+        return nameA.localeCompare(nameB);
+      });
+
+      data.forEach((country) => {
+        output += `<option value="${country.name.common}">${country.name.common}</option>`;
+      });
+      
+      selectDrop.innerHTML = output;
+    })
+    .catch((error) => {
+      console.error("Error fetching countries:", error);
+    });
+});
